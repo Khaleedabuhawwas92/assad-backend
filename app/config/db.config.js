aa2f20e0c246c0c15dd1ee1ac347f96011631689
Khaleedabuhawwas92 <42714078+Khaleedabuhawwas92@users.noreply.github.com>
2022-01-20 17:39:59 +0200

    omar and khale

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-   url: "mongodb://localhost:27017/work",
+   url: "mongodb+srv://doadmin:46A3198v5OH7Usfx@db-mongodb-fra1-60404-f9b2ed9f.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-fra1-60404&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
 };
